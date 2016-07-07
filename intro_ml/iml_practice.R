e22122d135777f0a057b89ced0b2e267b0f51f08
Slava Zimin <slzdev@gmail.com>
2016-07-07 08:06:10 -0400

    progress in chap 1 iml

---
+++ intro_ml/iml_practice.R
@@ -0,0 +1,4 @@
+#rm(list=ls());  setwd('~/Dropbox/cs/bigdata/datacamp/intro_ml'); source('iml_practice.R')
+
+#rm(list=ls());  setwd('C:/Users/zimine/Dropbox/cs/bigdata/datacamp/intro_ml'); source('iml_practice.R') 
+
