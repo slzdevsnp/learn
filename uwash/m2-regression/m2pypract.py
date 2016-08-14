5c55e55e5e7dcbbccf7326e48385c84b78b46650
Slava Zimin <slzdev@gmail.com>
2016-08-14 16:07:36 -0400

    rename modul1 to m1-intro

---
+++ uwash/m2-regression/m2pypract.py
@@ -0,0 +1,11 @@
+#import os ; os.chdir('c:/Users/zimine/Dropbox/cs/bigdata/coursera/uwash/modul2') ; execfile('m2pypract.py')
+
+#import os ; os.chdir('/Users/zimine/Dropbox/cs/bigdata/coursera/uwash/modul2') ; execfile('m2pypract.py')
+
+
+print os.getcwd()
+
+#import graphlab 
+import sframe as sf
+import numpy as np 
+import pandas as pnd 
