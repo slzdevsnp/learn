b81a5259bf015f2be6ff5cf9973cef6a5c7260e7
sviatoslav zimine <slzdev@gmail.com>
2016-09-15 13:18:24 +0200

    m1 prj wip

--- dasi/m1/m1w4_practice.R
+++ dasi/m1/m1w4_practice.R
@@ -74,12 +74,19 @@ selected_nzes2011 %>%
 group_by(jpartyvote, X_singlefav,sameparty) %>% summarise(count=n())
 )
 ## look at stats only for the same
+print('look at stats only for votes on the same party')
 print(
 selected_nzes2011 %>%
  group_by(jpartyvote, X_singlefav,sameparty) %>% 
  summarise(count=n()) %>%
  filter(sameparty=="same")
-
+)
+print('look at stats only for votes on NOT the same party')
+print(
+selected_nzes2011 %>%
+ group_by(jpartyvote,sameparty) %>% 
+ summarise(count=n()) %>%
+ filter(sameparty=="different")
 )
 
 
@@ -156,13 +163,106 @@ selected_nzes2011 %>%
 )
 #the above shows the problem so  the correct conversion is
 selected_nzes2011 <- selected_nzes2011 %>% 
-## redo the grouping check
   mutate(numlikenzf = as.numeric(as.character(jnzflike)))
+
+
 print(
 selected_nzes2011 %>% 
   group_by(jnzflike,numlikenzf) %>% 
   summarise(count=n())
 )
 
+print('show if people like NZ first more with their age')
 
+print(
+selected_nzes2011 %>%
+	 filter( !is.na(retiredage))  %>%
+	 group_by(retiredage) %>% 
+	 summarise(medlike=median(numlikenzf,na.rm=T) )
+)
 
+## doing the same with more age granularity
+require(data.table)
+ds <- data.table(selected_nzes2011)
+ds[, `:=`(ag_young=ifelse(jage < 25,1,0)
+	     ,ag_adult=ifelse(jage>=25 & jage<65,1,0)
+	     ,ag_retire=ifelse(jage>65,1,0) ) ]
+ds[, numlikenzf := as.numeric(as.character(jnzflike))]
+ds[!is.na(jage), .(cnt=.N, medi_like=median(numlikenzf, na.rm=T))
+    ,by=.(ag_young,ag_adult, ag_retire)]
+
+###############################################
+##### szi #### revisiting the first question
+###############################################
+##adding new column
+
+selected_nzes2011 <- selected_nzes2011 %>%
+mutate(sameparty = ifelse(jpartyvote == X_singlefav, "same", "different"))
+
+## look at stats only for the same
+print('look at stats only for votes on the same party')
+print(
+selected_nzes2011 %>%
+ group_by(jpartyvote, X_singlefav,sameparty) %>% 
+ summarise(count=n()) %>%
+ filter(sameparty=="same")
+)
+print('look at stats only for votes on NOT the same party')
+print(
+selected_nzes2011 %>%
+ group_by(jpartyvote,sameparty) %>% 
+ summarise(count=n()) %>%
+ filter(sameparty=="different")
+)
+
+### new var from jdiffvoting on those who beleive voting has influence
+selected_nzes2011 <- selected_nzes2011 %>%
+mutate(votinflu = factor(ifelse(jdiffvoting %in% 
+	   c("Voting can make a big difference to what happens"
+	   	 ,"Voting can make a reasonable amount of difference to what happens"
+	   	 ,"Voting can make some difference to what happens")
+	 , "influence", "no influence")) )
+
+print('look at stats only for votes on NOT the same party vote influence vs not influence')
+print(
+selected_nzes2011 %>%
+ group_by(jpartyvote,sameparty, votinflu) %>% 
+ summarise(count=n()) %>%
+ filter(sameparty=="different")
+)
+print('look at stats only for votes on the same party invluence vs not influence')
+print(
+selected_nzes2011 %>%
+ group_by(jpartyvote, X_singlefav,sameparty, votinflu) %>% 
+ summarise(count=n()) %>%
+ filter(sameparty=="same")
+)
+
+print('folks with  influence')
+print(
+selected_nzes2011 %>%
+ group_by(sameparty, votinflu) %>%
+ summarise(count=n()) %>%
+ filter(votinflu =="influence" & !is.na(sameparty)) 
+)
+
+print('folks with no influence')
+print(
+selected_nzes2011 %>%
+ group_by(sameparty, votinflu) %>%
+ summarise(count=n()) %>%
+ filter(votinflu =="no influence" & !is.na(sameparty)) 
+)
+
+print('lbl')
+print(
+selected_nzes2011 %>%
+ group_by(sameparty, votinflu) %>%
+ summarise(count=n()) %>%
+ filter( !is.na(sameparty)) 
+)
+print(
+xtabs(~sameparty+votinflu, data=selected_nzes2011)
+)
+
+#end 
