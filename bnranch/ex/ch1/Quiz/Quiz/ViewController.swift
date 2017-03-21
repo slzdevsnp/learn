ca9e57282b1974e4577487a2b364c44082549f53
Slava Zimin <slzdev@gmail.com>
2017-03-21 13:10:38 +0100

    ch2 ok

--- bnranch/ex/ch1/Quiz/Quiz/ViewController.swift
+++ bnranch/ex/ch1/Quiz/Quiz/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7",
+        "What is the capital of Vermont?",
+        "What is cognact made from"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    ///first question loaded after the app start
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func showNextQuestion( _ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question  = questions[currentQuestionIndex]
+        questionLabel.text  = question
+        answerLabel.text    = "???"
     }
-
-
+    
+    @IBAction func showAnswer( _ sender: UIButton){
+    
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text    = answer
+    }
+    
 }
 
