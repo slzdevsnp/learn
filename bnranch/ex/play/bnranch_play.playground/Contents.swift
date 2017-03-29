f975165ccbbaa7e7d5e0ac2242069969f0153b45
Slava Zimin <slzdev@gmail.com>
2017-03-29 12:35:04 +0200

    ranch ch3 ch4 ok

---
+++ bnranch/ex/play/bnranch_play.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("\(str)")
+
+let s = "1.34"
+let s1 = "??"
+let dbl = Double(s)
+let dbl1 = Double(s1)
+
+let i1:Int?
+let i2:Int?
+
+i1 = 10
+i2 = 20
+
+if let a = i1 , let b = i2 {
+    print ("ok")
+}
+
+
