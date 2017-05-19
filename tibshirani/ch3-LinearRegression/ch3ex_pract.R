e5921276c7a27afd138756dcf05ff511ee1d5c9f
Slava Zimin <slzdev@gmail.com>
2017-05-19 17:10:44 +0200

    ch3 ex applied wip1

--- tibshirani/ch3-LinearRegression/ch3ex_pract.R
+++ tibshirani/ch3-LinearRegression/ch3ex_pract.R
@@ -13,4 +13,94 @@ print(summary(lmfit1))
 plot(mpg~horsepower,data=Auto, cex=0.3)
 abline(lmfit1, col=2)
 #i. we have a low p-value coef for horsepower variable  i.e. a relation between response and predictor exists
-#ii. 
+#ii.  the rse = 4.906 the mean value of responnse is 23.45. so  we have a percentage error 4.906/23.45 = 20 %
+# R^2 = 0.6  So the model does not fit the data very closely
+
+#ex9
+Auto<-read.csv("../data/Auto.csv", header=T, na.strings="?")
+summary(Auto$horsepower)
+
+
+cor(Auto[,1:8], use="complete.obs")
+
+lmfit3<-lm(mpg~.-name+cylinders:displacement, data=Auto)
+
+#9.e
+# cylinders:displacement
+# cylincders:horsepower
+# cylinders:year
+
+##--cylinders:weight
+##--cylinders:origin 
+
+
+##-displacement:horsepower
+##- displacement:weight
+
+#9.f
+
+##displacement helps  log(x) and x^2 term 
+
+#horsepower :  log   sqrt  
+
+#weight : log , x^2
+
+ #ex 10 
+
+require(ISLR)
+data(Carseats)
+# str(Carseats)
+
+## pairs on Sales, Price, Urban, US
+pairs(Carseats[,c(1,6,10,11)], cex=0.1)
+lmfit101<-lm(Sales~Price+Urban+US, data=Carseats)
+print( summary(lmfit101) )
+
+#ex 11
+
+set.seed(1)
+x=rnorm(100)
+y<-2*x+rnorm(100)
+
+lmfit111<-lm(y~x-1)
+summary(lmfit111)
+
+
+##ex12 
+set.seed(1)
+x=rnorm(100)
+y<-2*x+runif(100)
+print(summary(lm(y~x-1)))
+print(summary(lm(x~y-1)))
+
+
+browser()
+#ex13
+set.seed(1)
+x<-rnorm(100, mean=0, sd=1) #default values
+#(b)
+eps<-rnorm(100, mean=0, sd=0.25) #residuals with smaller variance
+#c)
+y<- -1+0.5*x + eps
+#(d)
+plot(y~x)
+
+#e)
+lmfit131<-lm(y~x)
+print(summary(lmfit131))
+#(f)
+abline(lmfit131, col=2)
+abline(-1, 0.5, col=3)
+legend(-1, legend=c("model fit", "pop. regression"),col=2:3,lwd=3 )
+#(g)
+lmfit132<-lm(y~x+I(x^2))
+print(summary(lmfit132)) #lower  F-statistic
+
+plot(y~x)
+abline(lmfit131, col=2)
+points(x,predict(lmfit132, data.frame(x)) ,col=4, cex=0.2)
+legend(-1, legend=c("linear model fit", "quadratic model fit"),col=c(2,4),lwd=3 )
+
+# end
+#dev.off()
+
