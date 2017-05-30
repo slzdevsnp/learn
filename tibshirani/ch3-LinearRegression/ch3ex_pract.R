0addd5423b629e55279992f54ce3dde1a68f5da0
Slava Zimin <slzdev@gmail.com>
2017-05-31 00:52:40 +0200

    ch3 ex applied wip2

--- tibshirani/ch3-LinearRegression/ch3ex_pract.R
+++ tibshirani/ch3-LinearRegression/ch3ex_pract.R
@@ -1,6 +1,7 @@
 #rm(list=ls());setwd("~/Dropbox/cs/bigdata/lagunita/tibshirani/ch3-LinearRegression/");source("ch3ex_pract.R")
 
 
+
 #ch3 ex applied helper code
 
 #ex 8
@@ -74,7 +75,7 @@ print(summary(lm(y~x-1)))
 print(summary(lm(x~y-1)))
 
 
-browser()
+#browser()
 #ex13
 set.seed(1)
 x<-rnorm(100, mean=0, sd=1) #default values
@@ -101,6 +102,56 @@ abline(lmfit131, col=2)
 points(x,predict(lmfit132, data.frame(x)) ,col=4, cex=0.2)
 legend(-1, legend=c("linear model fit", "quadratic model fit"),col=c(2,4),lwd=3 )
 
-# end
+
+##ex 14
+
+set.seed(1)
+x1<-runif(100)
+x2<-0.5*x1 + rnorm(100)/10
+y<-2+2*x1 +0.3*x2+rnorm(100)
+
+#14.c
+lmfit141<-lm(y~x1+x2)
+print(summary(lmfit141))
+
+# 14.d
+lmfit142<-lm(y~x1)
+print(summary(lmfit142))
+## ans: due to low p-value for x1 coef we can reject null hypo H_0 : \beta_1 = 0
+
+# 14.e
+lmfit143<-lm(y~x2)
+print(summary(lmfit143))
+## ans: due to low p-value for x1 coef we can reject null hypo H_0 : \beta_2 = 0
+
+# 14.g
+x1 <- c(x1, 0.1)
+x2 <- c(x2, 0.8) #this is an outlier seen on the plot
+y  <- c(y, 6) 
+
+plot(x1,x2)
+
+par(mfrow=c(3,2))
+plot(lmfit141, cex=0.3)
+
+plot(predict(lmfit141), rstudent(lmfit141)) # for outliers  (rstudent studentized resitulas shoud stay in [-3;3] )
+
+lmfit141<-lm(y~x1+x2) #redowing
+print(summary(lmfit141))
+
+dev.new()
+
+par(mfrow=c(3,2))
+plot(lmfit141, cex=0.3)
+plot(predict(lmfit141), rstudent(lmfit141))
+
+# ans. model (c) slighly larger F-statistic and slightly larger R^2
+#  2. according to studentized residual chart the new point is not  and outlier  
+#  and it is a highly leverated point as its leverage statistics greatly exceeds  vlaue (p+1)/n = 3/101 = 0.03
+
+
+
+
+### end
 #dev.off()
 
