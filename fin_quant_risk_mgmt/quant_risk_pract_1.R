2af090825e9aaee9bad822f518933ba180f7651b
Slava Zimin <slzdev@gmail.com>
2018-06-27 12:37:30 +0200

    add practice file in quant risk

--- fin_quant_risk_mgmt/quant_risk_pract_1.R
+++ fin_quant_risk_mgmt/quant_risk_pract_1.R
@@ -518,5 +518,49 @@ lossop <- function(xseries,r=0.01, K=100, T=1, sigma=0.2,S=100){
   return(ll)
 }
 
+# Calculate the first loss
+lossop(c(-0.1,-0.1),S=80, sigma=0.2)
 
+# Calculate the second loss
+lossop(c(-0.1, 0.1), S=100, sigma=0.2)
+
+# Create and plot hslosses
+hslosses <- lossop(returns,sigma=0.2,S=100)
+
+plot(hslosses)
+
+
+# Form a Q-Q plot of hslosses against normal
+qqnorm(hslosses)
+qqline(hslosses)
+# Plot the sample acf of raw data and absolute values in hslosses
+acf(hslosses)
+acf(abs(hslosses))
+
+### Estimating VaR and ES for option portfolio
+paste("non-parametric calculations from loss simulated data")
+# Estimate the 99.5% percentile of the distribution
+paste("99.5% VAR",quantile(hslosses, 0.995))
+
+# Estimate the 99.5% ES
+paste("99.5% ES from data",mean(hslosses[hslosses >= quantile(hslosses, 0.995)]))
+
+paste("var and ES if losses are thought to be normally distributed")
+# Estimate the mean and standard deviation of hslosses
+mu<-mean(hslosses)
+sigma<-sd(hslosses)
+
+# Compute the 99.5% quantile of a normal distribution
+paste("99.5% VAR from normal distribution",qnorm(0.995, mean=mu, sd=sigma))
+
+# Compute the 99.5% ES of a normal distribution
+paste("99.5 % ES from normal distribution",ESnorm(0.995, mu=mu, sd=sigma))
+
+
+#### computing weekly option portflio var 
+## convert daily returns to weekly
+returns_w <- apply.weekly(returns, colSums) 
+hslosses_w <- lossop(returns_w, sigma=0.25, S=120)
+paste("99 % var for weekly portfolio returns for S=120 and sigma=0.25")
+quantile(hslosses_w, 0.99)
 
